de2faa7385de5da75c7f304b849bb3bb431bc360
Harsh Kumar <hemenderkumar3000@gmail.com>
2025-08-02 22:29:48 +0530

    **Infinite Scroll Loader**

--- my-tube/src/modules/studio/views/studio-view.tsx
+++ my-tube/src/modules/studio/views/studio-view.tsx
@@ -2,7 +2,11 @@ import { VideoSection } from "../sections/video-section"
 
 export const StudioView = () => {
     return (
-        <div>
+        <div className="flex flex-col gap-y-6 pt-2.5">
+            <div className="p-4">
+                <h1 className="text-2xl font-bold">Channel Content</h1>
+                <p className="text-xs text-muted-foreground">Manage your channel content and videos</p>
+            </div>
             <VideoSection/>
         </div>
     )
